c1c431b5c5f34baef9a7f11af372e1562307831c
Hossein Moghimian <hossein@Hosseins-MacBook-Air.local>
2025-08-08 12:45:00 -0400

    Gillespie complete

--- model/sde.py
+++ model/sde.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def rep_stim_maker(num_repeat,total_time):
+    num_reps = int(np.floor(total_time/num_repeat))
+    ons = np.repeat(1,num_reps)
+    offs = np.repeat(0,num_reps)
+    tile = np.hstack((ons,offs))
+    stim_vec = np.tile(tile,int(num_repeat))
+    stim_vec = stim_vec[:total_time]
+    return stim_vec
 
 
 delta = 0.01
@@ -16,12 +24,11 @@ params = [alpha , k , n , tau_delay , h1 , h2 ,c2 , delta]
 num_steps = 200
 ons = np.repeat(1,50)
 offs = np.repeat(0,50)
-stim_vec = np.random.choice(2,num_steps, p =[0.55,.45])
-stim_vec = np.hstack((ons,offs,ons,offs))
+#stim_vec = np.random.choice(2,num_steps, p =[0.75,0.25])
+stim_vec = rep_stim_maker(4,200)
+stim_vec2 = np.hstack((stim_vec,np.zeros(100),np.ones(50),np.zeros(50)))
 X_0 = [0, np.round(np.random.poisson(h1/h2)),0] 
 
-
-t_max = 1000
 
 def gillespie_CcaSR(init_con,params,stim_vec, t_max):
     # Initialize
@@ -54,12 +61,13 @@ def gillespie_CcaSR(init_con,params,stim_vec, t_max):
     while t < t_max:
         a = np.array([f(X,t) for f in prop_funs])
         a0 = a.sum()
-        if a0 == 0:
+        if a0 <= 1e-2:
             break  # No reactions can occur
 
         # Time to next reaction
-        tau = np.random.exponential(1 / a0)
+        tau = np.random.exponential(np.abs(1 / a0))
         t += tau
+        if t > t_max: break
 
         # Choose reaction
         r = np.random.uniform(0, a0)
@@ -68,7 +76,7 @@ def gillespie_CcaSR(init_con,params,stim_vec, t_max):
         # Update species
         X += sto_mat[j]
         Outputs = X.copy()
-        #Outputs[2] = Outputs[2] + np.random.normal(0,(0.05*Outputs[2]))
+        #Outputs[2] = Outputs[2] + np.random.normal(0,(0.0005*Outputs[2]))
         # Record
         time.append(t)
         history.append(Outputs.copy())
@@ -78,21 +86,30 @@ def gillespie_CcaSR(init_con,params,stim_vec, t_max):
 
 
 fig, ax = plt.subplots(figsize=(8, 4))
-for i, val in enumerate(stim_vec):
+for i, val in enumerate(stim_vec2):
     x_start = i * 5
     x_end = x_start + 5
     color = 'green' if val == 1 else 'red'
     ax.axvspan(x_start, x_end, facecolor=color, alpha=0.2)
 
 
-t_max = 1000
-time, X_vals = gillespie_CcaSR(X_0, params, stim_vec, t_max)
-ax.step(time,X_vals[:,2], where='post')
+t_max = 2000
+Xs = []
+lens = []
+for i in range(5):
+    time, X_vals = gillespie_CcaSR(X_0, params, stim_vec2, t_max)
+    Xs.append(X_vals[:,2])
+    ax.step(time,Xs[i], where='post',color = 'blue',alpha= 0.5)
+    lens.append(len(X_vals[:,2]))
+#Xs = np.array(Xs)
+#X_mean = np.mean(Xs,axis = 0)
+
+
+
 
 
 plt.xlabel("Time")
 plt.ylabel("X (molecule count)")
-plt.title("Gillespie Simulation of Birth-Death Process")
 ax.grid(True)
 plt.tight_layout()
 plt.show()
