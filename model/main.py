d37ed53b0fa55ef6f3135c1bd3ae15b98d264bb2
Hossein Moghimian <hossein@crc-dot1x-nat-10-239-206-152.bu.edu>
2025-08-25 11:53:05 -0400

    organized and online-exn added

--- model/main.py
+++ model/main.py
@@ -1,3 +1,44 @@
-###
-###
-###
+import models
+import utils
+
+import numpy as np
+
+stim_vec2 = utils.repetitive_stim_maker(2,80)
+
+delta = 0.01
+alpha = 1
+k = 0.4851
+h1 = 2.3435*0.0303
+h2 = 0.0303
+tau_delay = 12
+n=3.6
+c2 = 0.0631
+params = [alpha , k , n , tau_delay , h1 , h2 ,c2 , delta]
+#stim_vec = np.random.choice(2,80)
+stim_vec =[stim_vec2,stim_vec2[::-1],stim_vec2]
+t_max = 400
+sampling = 10
+mega_res = {f'cell {i+1}':[] for i in range(10)}
+Models = {}
+
+for i in range(10):
+    model = models.CcaSR(params,t_max,sampling)
+    model.init_rxn()
+    Models[f'cell {i+1}'] = model
+
+for i in range(10):
+    model = Models[f'cell {i+1}']
+    new_state = None
+    for j,stim in enumerate(stim_vec):
+        results , new_state = model.run_online_rxn(updates=new_state,stim_vec=stim)
+        mega_res[f'cell {i+1}'].append(results)
+
+for i in range(10):
+    model = Models[f'cell {i+1}']
+    new_state = model.give_updates(mega_res[f'cell {i+1}'][-1])
+    for j,stim in enumerate(stim_vec):
+        results , new_state = model.run_online_rxn(updates=new_state,stim_vec=stim)
+        mega_res[f'cell {i+1}'].append(results)
+
+
+utils.plot_w_bckgrnd(mega_res,np.concatenate(stim_vec,stim_vec),t_max)
---
+++ model/utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def repetitive_stim_maker(num_repeat,total_time,off_first = False):
+    '''
+    num_repeat: number of segments each made of repetitive cycles (e.g., if 2, [11110000])
+    total_time: span of the stimulation experiment. For example, if 20, it means 20 cycles (and for a 5 min stim sampling, 100 mins).
+    off_first: bool whether stim starts with off signal or not. Defaults is False
+    **Example**:
+    num_repeat = 4 and total_time = 60 means [[15 ones and 15 zeros, 15 ones and 15 zeros]
+    '''
+    num_reps = int(np.floor(total_time/num_repeat))
+    ons = np.repeat(1,num_reps)
+    offs = np.repeat(0,num_reps)
+    tile = np.hstack((offs,ons)) if off_first else np.hstack((ons,offs)) 
+    stim_vec = np.tile(tile,int(num_repeat))
+    stim_vec = stim_vec[:total_time]
+    return stim_vec
+
+
+
+def plot_w_bckgrnd(mega_res,stim_vec_tot,t_max,sampling=10,save=None):
+    '''
+    mega_res: dictionary with all cell simulation results
+    stim_vec_tot: list of arrays of stims in the form of 1 and 0 for the whole history of simulation
+    t_max: total time of each simulation 
+    sampling: data sampling during simulation (default = 10) 
+    save: a dictionary with information about saving the figure. Default is None which causes only showing the figure
+    '''
+    fig, ax = plt.subplots(figsize=(8, 4))
+    for j in range(len(stim_vec_tot)):
+        for i, val in enumerate(stim_vec_tot[j]):
+            x_start = j*t_max*sampling + i * 5*sampling
+            x_end = x_start + 5*sampling
+            color = 'green' if val == 1 else 'red'
+            ax.axvspan(x_start, x_end, facecolor=color, alpha=0.2)
+    for k in range(len(mega_res.keys())):
+        results = mega_res[f'cell {k+1}']
+        for i in range(len(results)):
+            ax.plot(np.arange(t_max*i*sampling,t_max*(i+1)*sampling),(results[i]['F']),color='b',linewidth = 0.5)
+
+    if save is not None:
+        path = save['path']
+        fig.savefig(path,dpi=300,format = 'svg')
+    else:    
+        ax.set_xlabel('Time (min)')
+        ax.set_ylabel('GFP (molecule count)')
+        plt.tight_layout()
+        plt.show()  
