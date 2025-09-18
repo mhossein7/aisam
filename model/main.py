243d5a7384f91f50d2705a2aca92633386a2f593
Hossein Moghimian <hossein@crc-dot1x-nat-10-239-206-152.bu.edu>
2025-09-18 11:15:25 -0400

    added ODE and refined main

--- model/main.py
+++ model/main.py
@@ -20,7 +20,7 @@ c2 = 0.0631
 params = [alpha , k , n , tau_delay , h1 , h2 ,c2 , delta]
 params_tet = [alpha , beta, k_tet , k , n ,n_tet, tau_delay , h1 , h2 , c2,delta]
 tot_stim_vec = []
-t_max = 270
+t_max = 600
 sampling = 10
 mega_res = {f'cell {i+1}':[] for i in range(10)}
 Models = {}
@@ -30,7 +30,7 @@ for i in range(10):
     model.init_rxn()
     Models[f'cell {i+1}'] = model
 
-for t in tqdm(range(10)):
+for t in tqdm(range(2)):
     stim_vec = [utils.repetitive_stim_maker(2,int(t_max/5))]
     for i in range(10):
         model = Models[f'cell {i+1}']
@@ -40,9 +40,14 @@ for t in tqdm(range(10)):
     tot_stim_vec.append(stim_vec)
 
 
-fig,ax = plt.subplots(2,1,figsize = (6,4))
+for i in range(10):
+    temp = mega_res[f'cell {i+1}'][-1]
+    mega_res[f'cell {i+1}'].clear()
+    mega_res[f'cell {i+1}'].append(temp)
+    
+fig,ax = plt.subplots(1,1,figsize = (6,4))
 
-utils.plot_w_bckgrnd(mega_res,np.concatenate(tot_stim_vec),t_max,line_color= 'b',axes=True,ax_out=ax[0])
-utils.plot_w_bckgrnd(mega_res,np.concatenate(tot_stim_vec),t_max,'T',line_color = 'r',axes=True,ax_out = ax[1])
+utils.plot_w_bckgrnd(mega_res,[np.concatenate(tot_stim_vec[-1])],t_max,line_color= 'b',axes=True,ax_out=ax)
+#utils.plot_w_bckgrnd(mega_res,np.concatenate(tot_stim_vec),t_max,'T',line_color = 'r',axes=True,ax_out = ax[1])
 plt.tight_layout()
 plt.show()
